a517ceb5ce33c83b953458669e1291a3d0ac1b1d
Amen Muhammad <amenmuhhammad@gmail.com>
2026-04-28 16:15:50 +0400

    Remove unused variable initializations

--- main.py
+++ main.py
@@ -3,11 +3,3 @@ from core import *
 from operations import * 
 from functions import *
 
-x=Variable("x") 
-y=Variable("y")
-z=Variable("z")
-e=Euler()
-pi=Pi()
-
-a = x**Sin(x)
-print(a.orderdiff(x,3))
